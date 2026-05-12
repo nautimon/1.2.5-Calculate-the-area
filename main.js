92564d56937697e0403c0355715ff6110760c8bd
nautimon <nautisignup@gmail.com>
2026-05-12 04:01:07 +0000

    Calculate area

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Find the area of a rectangle, setting height and width
-var height;
-var width;
+var height=150;
+var width=250;
 
 // Calculate area from height and width
-var area;
+var area=height*width+("sq ft");
 
 console.log(area);
